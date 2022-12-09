7b56d48f6968b5aa5526d673e16a01ca4e2dc0c4
Giovanni Benato <benatogiovanni@gmail.com>
2022-12-09 10:05:35 +0100

    Added Poisson example

---
+++ Poisson/Poisson.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <string>
+
+#include "TH1D.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TPad.h"
+#include "TPaveStats.h"
+#include "TF1.h"
+
+// Works only up to n=170, after which it hits the max value allowed for a double and returns nan.
+double Factorial( double n ){
+    
+    if( n <= 1. )
+	return 1.;
+    else
+    	return n * Factorial(n-1.);
+}
+
+// Computes the logarithm of the factorial.
+// Useful if the factorial is divided by a similarly large number, as in the Poisson case.
+double LogFactorial( double n )
+{
+    if( n <= 1. )
+	return 0.;
+    return log(n) + LogFactorial(n-1.);
+}
+
+// This is the most straightforward implementation of the binomial coefficient.
+// It works only up to n=170, after which Factorial(n) returns nan.
+double BinomialCoefficient( double N, double k )
+{
+    return TMath::Binomial( N, k );
+    //return Factorial(N) / Factorial(k) / Factorial(N-k);
+}
+
+// Straightforward implementation of the binomial.
+// Works up to n=170.
+double Binomial( double n,
+		 double N,
+		 double p,
+		 double A=1 )
+{
+    return A * BinomialCoefficient( N, n ) * pow( p, n ) * pow( 1.-p, N-n );
+}
+
+// Smarter implementation of hte Binomial,
+// which minimizes the number of calculations and includes the log-based
+// calculation of the binomial coefficient reported above.
+// It has a relative precision of ~1e-15 with respect to the straightforward implementation.
+double SmartBinomial( double k, double n, double p, double A=1 )
+{
+    // Binomial coefficient
+    double sum_1 = 0;
+    double sum_2 = 0;
+    for( double i=(int)(n-k+1); i<=n; i+=1. )
+	sum_1 += log(i);
+    for( double i=1.; i<=k; i+=1. )
+	sum_2 += log(i);
+    // Exponential terms
+    double exp_1 = k * log(p);
+    double exp_2 = (n-k) * log(1.-p);
+
+    return A * exp( sum_1 - sum_2 + exp_1 + exp_2 );
+}
+
+// Straightforward implementation of the Poisson distribution.
+// Fails for n>170 due to the factorial issue.
+double Poisson( double n,
+		double lambda,
+		double A=1 )
+{
+    return A * pow( lambda, n ) * exp(-lambda) / Factorial(n);
+}
+
+// Smarter way to implement the Poisson distribution, which works for n>170.
+double SmartPoisson( double n,
+		     double lambda,
+		     double A=1 )
+{
+    return A * exp( -lambda + n * log(lambda) - LogFactorial(n) );
+}
+
+int main()
+{
+    // Prepare random number machinery
+    std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+    double minT = 0.;
+    double maxT = 1.e3;// This is just a "random" choice, and has no influence on the result
+    std::uniform_real_distribution<> dis(minT,maxT);
+    
+
+    std::vector<double> P{0.01,0.05,0.1,0.5};// Fraction of successes
+    std::vector<double> N{10,100,1000};
+
+    size_t nCases = P.size() * N.size();
+    std::vector<TH1D*> dataHisto(nCases);
+    std::vector<TH1D*> binomialHisto(nCases);
+    std::vector<TH1D*> poissonHisto(nCases);
+
+    int c=0;// index of histogram vectors
+    int M=100000;// Number of toy-MC experiments
+    // Loop over N and P
+    for( auto& n: N )
+	for( auto& p: P )
+	    {
+		double dT = p*(minT+maxT) - minT;
+		double lambda = p * n;// Expectation value for Poisson distro
+
+		// Prepare histograms
+		double expectedN = p*n;
+		double min = std::max( -0.5+(int)(expectedN-7.*sqrt(expectedN)), -0.5 );
+		double max = 0.5+(int)(expectedN+7.*sqrt(expectedN));
+		int nBins = max-min;
+		// Histograms for toy-MC data
+		std::string name( "Data N=" + std::to_string((int)n) + " p=" + std::to_string(p) );
+		dataHisto[c] = new TH1D( name.c_str(), name.c_str(), nBins, min, max );
+		// Histogram for binomial distro
+		name = "Binomial N=" + std::to_string((int)n) + " p=" + std::to_string(p);
+		binomialHisto[c] = new TH1D( name.c_str(), name.c_str(), nBins, min, max );
+		// Histogram for Poisson distro
+		name = "Poisson N=" + std::to_string((int)n) + " p=" + std::to_string(p);
+		poissonHisto[c] = new TH1D( name.c_str(), name.c_str(), nBins, min, max );
+		// Graphic settings
+		dataHisto[c]->GetXaxis()->SetTitle("k");
+		dataHisto[c]->GetXaxis()->SetTitle("Entries");
+		binomialHisto[c]->SetLineColor(2);
+		poissonHisto[c]->SetLineColor(4);
+
+		// Loop over toy-MC experiments
+		for( int i=0; i<M; i++ )
+		    {
+			// Generate n time values
+			std::vector<double> time(n);
+			for( auto& t: time )
+			    t = dis(gen);
+			// count cases with t<dT
+			int k=0;
+			for( auto& t: time )
+			    if( t < dT ) k++;
+			// Fill data histogram
+			dataHisto[c]->Fill(k);
+		    }
+
+		// Fill binomial and Poisson distributions
+		for( int b=1; b<nBins; b++ )// The bin number of ROOT TH1D starts from 1.
+		    {
+			double k = binomialHisto[c]->GetBinCenter(b);
+			double tmp = SmartBinomial(k,n,p,M);
+			if( !std::isnan(tmp) )// Just to avoid some rare failure of SmartBinomial()
+			    binomialHisto[c]->SetBinContent( b, tmp );
+			poissonHisto[c]->SetBinContent( b, SmartPoisson(k,lambda,M) );
+		    }
+		
+		c++;
+	    }
+
+    // Draw
+    TApplication* app = new TApplication( "app", NULL, 0 );
+    TCanvas* can = new TCanvas( "can", "can", 1600, 900 );
+    can->Divide(P.size(),N.size());
+    for( size_t c=0; c<nCases; c++ )
+	{
+	    can->cd(c+1);
+	    dataHisto[c]->Draw();
+	    binomialHisto[c]->Draw("SAME");
+	    poissonHisto[c]->Draw("SAME");
+	}
+    app->Run(kTRUE);
+
+    return 0;
+}
