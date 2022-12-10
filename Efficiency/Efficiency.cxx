b769491a3bc9abbc19d450543fc596c4eae2cf40
Giovanni Benato <benatogiovanni@gmail.com>
2022-12-10 12:09:22 +0100

    Created example for fitting efficiency with Binomial, Poisson and Gaussian likelihood

---
+++ Efficiency/Efficiency.cxx
@@ -0,0 +1,199 @@
+// ***************************************************************
+// This file was created using the bat-project script.
+// bat-project is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://mpp.mpg.de/bat
+// ***************************************************************
+#include <random>
+
+#include "Efficiency.h"
+
+#include <BAT/BCConstantPrior.h>
+// #include <BAT/BCMath.h>
+
+// ---------------------------------------------------------
+Efficiency::Efficiency( const std::string& name,
+			const int n,
+			const double eff )
+    : BCModel(name)
+{
+    fMethod = FitMethod::kPoisson;
+    
+    fN          = n;
+    fMinE       = 1.;// keV
+    fMaxE       = 20.;// keV
+    fThreshold  = 3.;// keV
+    fSigma      = 1.;
+    fEfficiency = eff;
+    fEfficiencyCurve = TF1( "EfficiencyCurve", "0.5*[0]*(1.+TMath::Erf((x-[1])/[2]))", -1.+fMinE, 1.+fMaxE );
+    fEfficiencyCurve.SetParameter(0,fEfficiency);
+    fEfficiencyCurve.SetParameter(1,fThreshold);
+    fEfficiencyCurve.SetParameter(2,fSigma);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0,1);
+    
+    for( int e=fMinE; e<=fMaxE; e++ )
+	{
+	    double k = 0;
+	    for( int i=0; i<fN; i++ )
+		if( dis(gen) < EvaluateEfficiency( e,
+						   fEfficiency,
+						   fThreshold,
+						   fSigma ) )
+		    k += 1.;
+	    
+	    fK[e] = k;
+	    //std::cout << e << "\t" << k << std::endl;
+	}
+
+
+    // Add parameters
+    double minEff = std::max( 0., 0.9*fEfficiency );
+    double maxEff = std::min( 1., 1.1*fEfficiency );
+    AddParameter( "Efficiency", minEff, maxEff, "#epsilon", "" );
+    GetParameters().Back().SetPrior( new BCConstantPrior() );
+    GetParameters().Back().SetNbins(300);
+
+    AddParameter( "Threshold", 0.9*fThreshold, 1.1*fThreshold, "E_{thr}", "[keV]" );
+    GetParameters().Back().SetPrior( new BCConstantPrior() );
+    GetParameters().Back().SetNbins(300);
+
+    AddParameter( "Sigma", 0.9*fSigma, 1.1*fSigma, "#sigma", "[keV]" );
+    GetParameters().Back().SetPrior( new BCConstantPrior() );
+    GetParameters().Back().SetNbins(300);
+
+    
+    // Define parameters here in the constructor.
+    // Also define their priors, if using built-in priors.
+    // For example:
+    // AddParameter("mu", -2, 1, "#mu", "[GeV]");
+    // GetParameters.Back().SetPrior(new BCGaussianPrior(-1, 0.25));
+
+    // Define observables here, too. For example:
+    // AddObservable("mu_squared", 1, 4, "#mu^{2}", "[GeV^{2}]");
+}
+
+// ---------------------------------------------------------
+Efficiency::~Efficiency()
+{
+    // destructor
+}
+
+// ---------------------------------------------------------
+double Efficiency::LogLikelihood(const std::vector<double>& pars)
+{
+    // return the log of the conditional probability p(data|pars).
+    // This is where you define your model.
+    // BCMath contains many functions you will find helpful.
+
+    double logL = 0.;
+    if( fMethod == FitMethod::kBinomial )
+	{
+	    double E, k, p;
+	    for( auto& it: fK )
+		{
+		    E = it.first;
+		    k = it.second;
+		    p = EvaluateEfficiency( E, pars[0], pars[1], pars[2] );
+		    logL += LogBinomial( k, fN, p );
+		}
+	}
+    else if( fMethod == FitMethod::kPoisson )
+	{
+	    double E, k, lambda;
+	    for( auto& it: fK )
+		{
+		    E = it.first;
+		    k = it.second;
+		    lambda = EvaluateEfficiency( E, pars[0], pars[1], pars[2] ) * fN;
+		    logL += LogPoisson( k, lambda );
+		}
+	}
+    else if( fMethod == FitMethod::kChiSquare )
+	{
+	    double E, k, mu;
+	    for( auto& it: fK )
+		{
+		    E = it.first;
+		    k = it.second;
+		    mu = EvaluateEfficiency( E, pars[0], pars[1], pars[2] ) * fN;
+		    logL += LogChi2( k, mu );
+		}
+	}
+    
+    return logL;
+}
+
+// ---------------------------------------------------------
+// double Efficiency::LogAPrioriProbability(const std::vector<double>& pars)
+// {
+//     // return the log of the prior probability p(pars)
+//     // If you use built-in priors, leave this function commented out.
+// }
+
+// ---------------------------------------------------------
+// void Efficiency::CalculateObservables(const std::vector<double>& pars)
+// {
+//     // Calculate and store obvserables. For example:
+//     GetObservable(0) = pow(pars[0], 2);
+// }
+
+double Efficiency::EvaluateEfficiency( double E,
+				       double asympEff,
+				       double threshold,
+				       double sigma )
+{
+    return 0.5 * asympEff * ( 1. + erf( ( E-threshold ) / sigma ) );
+}
+
+double Efficiency::LogBinomial( double k, double n, double p )
+{
+    // To save time, we drop the Binomial coefficient, which does not depend on p.
+    // This reduces the CPU time by a factor ~20
+    double exp_1 = k * log(p);
+    double exp_2 = (n-k) * log(1.-p);
+
+    return exp_1 + exp_2;
+
+    // Full formula, including the Binomial coefficient (sum1-sum2)
+    //double sum_1 = 0;
+    //double sum_2 = 0;
+    //for( double i=(int)(n-k+1); i<=n; i+=1. )
+    //    sum_1 += log(i);
+    //for( double i=1.; i<=k; i+=1. )
+    //    sum_2 += log(i);
+    //double exp_1 = k * log(p);
+    //double exp_2 = (n-k) * log(1.-p);
+    //return sum_1 - sum_2 + exp_1 + exp_2;
+}
+
+// Computes the logarithm of the factorial.
+double Efficiency::LogFactorial( double n )
+{
+    if( n <= 1. )
+        return 0.;
+    return log(n) + LogFactorial(n-1.);
+}
+
+double Efficiency::LogPoisson( double n,
+			       double lambda )
+{
+    // Faster formula, neglecting the factorial term which does not depend on lambda
+    return -lambda + n * log(lambda);
+    
+    // Full formula, including factorial term
+    //return exp( -lambda + n * log(lambda) - LogFactorial(n) );
+}
+
+double Efficiency::LogChi2( double n, double mu )
+{
+    return -0.5 * pow( n-mu, 2. ) / mu - log( sqrt(2.*M_PI) * mu );
+}
+
+void Efficiency::SetFitMethod( FitMethod method )
+{
+    fMethod = method;
+    
+    return;
+}
---
+++ Efficiency/runEfficiency.cxx
@@ -0,0 +1,185 @@
+// ***************************************************************
+// This file was created using the bat-project script
+// for project Efficiency.
+// bat-project is part of Bayesian Analysis Toolkit (BAT).
+// BAT can be downloaded from http://mpp.mpg.de/bat
+// ***************************************************************
+#include <vector>
+#include <string>
+
+#include <BAT/BCLog.h>
+
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TApplication.h"
+
+#include "Efficiency.h"
+
+int main()
+{
+    // open log file
+    BCLog::OpenLog("log.txt", BCLog::error, BCLog::error);
+    //BCLog::OpenLog("log.txt", BCLog::detail, BCLog::detail);
+
+    std::vector<int>    N{100,1000,10000};
+    std::vector<double> P{0.9,0.95,0.99};
+    int M = 1000;// number of toy-MC experiments
+    
+    size_t nCases = N.size() * P.size();
+    std::vector<TH1D*> h_eff_binomial( nCases );
+    std::vector<TH1D*> h_eff_poisson ( nCases );
+    std::vector<TH1D*> h_eff_chi2    ( nCases );
+    std::vector<TH1D*> h_threshold_binomial( nCases );
+    std::vector<TH1D*> h_threshold_poisson ( nCases );
+    std::vector<TH1D*> h_threshold_chi2    ( nCases );
+    std::vector<TH1D*> h_sigma_binomial( nCases );
+    std::vector<TH1D*> h_sigma_poisson ( nCases );
+    std::vector<TH1D*> h_sigma_chi2    ( nCases );
+    
+    int i=0;
+    std::string h_name;
+    int nBins = 1000;
+    for( auto& n: N )
+	for( auto& p: P )
+	    {
+		std::cout << n << "\t" << p << std::endl;
+ 
+		h_name = "Eff-binomial n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_eff_binomial[i] = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Eff-Poisson n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_eff_poisson[i]  = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Eff-Chi2 n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_eff_chi2[i]     = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Threshold-binomial n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_threshold_binomial[i] = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Threshold-Poisson n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_threshold_poisson[i]  = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Threshold-Chi2 n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_threshold_chi2[i]     = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Sigma-binomial n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_sigma_binomial[i] = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Sigma-Poisson n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_sigma_poisson[i]  = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+		h_name = "Sigma-Chi2 n=" + std::to_string(n) + " p=" + std::to_string(p);
+		h_sigma_chi2[i]     = new TH1D( h_name.c_str(), h_name.c_str(), nBins, 0.8, 1 );
+
+		h_eff_binomial[i]      ->SetLineColor(1);
+		h_threshold_binomial[i]->SetLineColor(1);
+		h_sigma_binomial[i]    ->SetLineColor(1);
+		h_eff_poisson[i]      ->SetLineColor(2);
+		h_eff_poisson[i]->SetFillColor(2);
+		h_threshold_poisson[i]->SetLineColor(2);
+		h_sigma_poisson[i]    ->SetLineColor(2);
+		h_eff_chi2[i]      ->SetLineColor(4);
+		h_threshold_chi2[i]->SetLineColor(4);
+		h_sigma_chi2[i]    ->SetLineColor(4);
+
+		double eff_binomial_weight = 0;
+		double eff_poisson_weight  = 0;
+		double eff_chi2_weight     = 0;
+		
+		for( int m=0; m<M; m++ )	    
+		    {
+			
+			// create new Efficiency object
+			Efficiency model( "Efficiency", n, p );
+			
+			// set precision
+			//model.SetPrecision(BCEngineMCMC::kHigh);
+			
+			//BCLog::OutSummary("Test model created");
+			model.SetFitMethod( Efficiency::FitMethod::kBinomial );
+			//model.MarginalizeAll(BCIntegrate::kMargMetropolis);
+			model.FindMode();
+			//model.FindMode(model.GetBestFitParameters());
+			//model.PrintSummary();
+			h_eff_binomial[i]      ->Fill( model.GetBestFitParameters()[0],
+						       1./pow(model.GetBestFitParameterErrors()[0],2.) );
+			h_threshold_binomial[i]->Fill( model.GetBestFitParameters()[1],
+						       1./pow(model.GetBestFitParameterErrors()[1],2.) );
+			h_sigma_binomial[i]    ->Fill( model.GetBestFitParameters()[2],
+						       1./pow(model.GetBestFitParameterErrors()[2],2.) );
+			eff_binomial_weight += 1./pow(model.GetBestFitParameterErrors()[0],2.);
+		    
+			model.SetFitMethod( Efficiency::FitMethod::kPoisson );
+			model.FindMode();
+			//model.FindMode(model.GetBestFitParameters());
+			//model.PrintSummary();
+			h_eff_poisson[i]      ->Fill( model.GetBestFitParameters()[0],
+						      1./pow(model.GetBestFitParameterErrors()[0],2.) );
+			h_threshold_poisson[i]->Fill( model.GetBestFitParameters()[1],
+						      1./pow(model.GetBestFitParameterErrors()[1],2.) );
+			h_sigma_poisson[i]    ->Fill( model.GetBestFitParameters()[2],
+						      1./pow(model.GetBestFitParameterErrors()[2],2.) );
+			eff_poisson_weight += 1./pow(model.GetBestFitParameterErrors()[0],2.);
+			
+			model.SetFitMethod( Efficiency::FitMethod::kChiSquare );
+			model.FindMode();
+			//model.FindMode(model.GetBestFitParameters());
+			//model.PrintSummary();
+			h_eff_chi2[i]      ->Fill( model.GetBestFitParameters()[0],
+						   1./pow(model.GetBestFitParameterErrors()[0],2.) );
+			h_threshold_chi2[i]->Fill( model.GetBestFitParameters()[1],
+						   1./pow(model.GetBestFitParameterErrors()[1],2.) );
+			h_sigma_chi2[i]    ->Fill( model.GetBestFitParameters()[2],
+						   1./pow(model.GetBestFitParameterErrors()[2],2.) );
+			eff_chi2_weight += 1./pow(model.GetBestFitParameterErrors()[0],2.);
+		    }
+
+		h_eff_binomial[i]->Scale(1./eff_binomial_weight);
+		h_eff_poisson[i]->Scale(1./eff_poisson_weight);
+		h_eff_chi2[i]->Scale(1./eff_chi2_weight);
+		i++;
+	    }
+
+    // Draw
+    TApplication* app = new TApplication( "app", NULL, 0 );
+    TCanvas* can = new TCanvas( "can", "can", 1600, 900 );
+    can->Divide(P.size(),N.size());
+    for( size_t c=0; c<nCases; c++ )
+	{
+	    can->cd(c+1);
+	    for( int b=1; b<=nBins; b++ )
+		{
+		    h_eff_poisson[c]->SetBinError(b,0);
+		    h_eff_binomial[c]->SetBinError(b,0);
+		    h_eff_chi2[c]->SetBinError(b,0);
+		}
+	    h_eff_poisson[c]->Draw("");
+	    h_eff_binomial[c]->Draw("same");
+	    h_eff_chi2[c]->Draw("same");
+	}
+    app->Run(kTRUE);
+    //////////////////////////////
+    // perform your analysis here
+
+    // Normalize the posterior by integrating it over the full parameter space
+    // model.Normalize();
+
+    // Write Markov Chain to a ROOT file as a TTree
+    // model.WriteMarkovChain(model.GetSafeName() + "_mcmc.root", "RECREATE");
+
+    // run MCMC, marginalizing posterior
+    //model.MarginalizeAll(BCIntegrate::kMargMetropolis);
+
+    // run mode finding; by default using Minuit
+    //model.FindMode(model.GetBestFitParameters());
+
+    // draw all marginalized distributions into a PDF file
+    //model.PrintAllMarginalized(model.GetSafeName() + "_plots.pdf");
+
+    // print summary plots
+    // model.PrintParameterPlot(model.GetSafeName() + "_parameters.pdf");
+    // model.PrintCorrelationPlot(model.GetSafeName() + "_correlation.pdf");
+    // model.PrintCorrelationMatrix(model.GetSafeName() + "_correlationMatrix.pdf");
+    // model.PrintKnowledgeUpdatePlots(model.GetSafeName() + "_update.pdf");
+
+    // print results of the analysis into a text file
+    //model.PrintSummary();
+
+    // close log file
+    BCLog::OutSummary("Exiting");
+    BCLog::CloseLog();
+
+    return 0;
+}
